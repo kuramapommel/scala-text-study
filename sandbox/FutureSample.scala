660d06628381216ab3f5ec540ac5c6741c99d33c
kuramapommel <pommel.pg@gmail.com>
2018-06-12 17:47:55 +0900

    use await

--- sandbox/FutureSample.scala
+++ sandbox/FutureSample.scala
@@ -1,36 +1,26 @@
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 object FutureSample extends App {
 
   val s = "Hello"
   val f: Future[String] = Future {
     Thread.sleep(1000)
+    println(s"[ThreadName] In Future: ${Thread.currentThread.getName}")
     s + " future!"
   }
 
   f.foreach { case s: String =>
+    println(s"[ThreadName] In Success: ${Thread.currentThread.getName}")
     println(s)
   }
 
   println(f.isCompleted)
 
-  Thread.sleep(5000)
+  Await.ready(f, 5000 millisecond)
 
+  println(s"[ThreadName] In App: ${Thread.currentThread.getName}")
   println(f.isCompleted)
-
-  val f2: Future[String] = Future {
-    Thread.sleep(1000)
-    throw new RuntimeException("わざと失敗")
-  }
-
-  f2.failed.foreach { case e: Throwable =>
-    println(e.getMessage)
-  }
-
-  println(f2.isCompleted)
-
-  Thread.sleep(5000)
-
-  println(f2.isCompleted)
 }
